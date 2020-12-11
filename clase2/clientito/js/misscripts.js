ba873619430c28cab5dfd5a1580115ab0d729cba
andy <arteysoft@gmail.com>
2020-12-11 12:55:15 -0300

    clase 2

---
+++ clase2/clientito/js/misscripts.js
@@ -0,0 +1,68 @@
+function ejemploGet(fnCallBack) {
+    fetch('eventos/server.php/doGet')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log('data = ', data);
+            fnCallBack(data);
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+}
+
+function ejemploPostFormulario() {
+    fetch('http://localhost:8080/mi', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Access-Control-Allow-Origin':'*'
+        },
+        body: 'a=1&b=2'
+        })
+        .then(function(response) {
+            console.log('response =', response);
+            return response.json();
+        })
+        .then(function(data) {
+            console.log('data = ', data);
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+}
+
+function ejemploPostObject(objEnviar) {
+    fetch('http://localhost:8080/clifrec', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*'
+        },
+        body: JSON.stringify(objEnviar)
+        })
+        .then(function(response) {
+            if (response.status === 201) {
+                alert('Se agrego correctamente');
+            }
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+}
+
+
+{
+    $('#btnGet').click(()=>{
+        ejemploGet(datoDelServidor=>{
+            alert(datoDelServidor.alumnos);
+
+            $('#selPruebas').html(
+                Mustache.render(
+                    $('#registrosCombo').html(), 
+                    { lista: datoDelServidor.alumnos})
+            );
+        });
+    });
+}
